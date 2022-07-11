151f28453e92c97f48c271035db0b000f52c1ab6
BingFengHung <joe94008@gmail.com>
2022-07-11 13:37:42 +0800

    <feat>: 加入 InstrucitonUtility 的輔助類別

--- src/InstructionSearch/MainViewModel.cs
+++ src/InstructionSearch/MainViewModel.cs
@@ -5,7 +5,11 @@ using System.Text;
 
 namespace InstructionSearch
 {
-    class MainViewModel
+    class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+
+        }
     }
 }
---
+++ src/InstructionSearch/Utilities/InstructionUtility.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace InstructionSearch
+{
+    class InstructionUtility
+    {
+        static string path = @".\InstructionSet";
+
+        static InstructionUtility()
+        {
+            if (Directory.Exists(path)) return;
+            Directory.CreateDirectory(path);
+        }
+
+        public static void CreateGroup(string groupName)
+        {
+            string group = Path.Combine(path, groupName);
+
+            if (Directory.Exists(group)) return;
+            Directory.CreateDirectory(group);
+        }
+
+        public static void WriteInstruction(string path, List<string> content)
+        {
+            File.WriteAllLines(path, content);
+        }
+
+        public static List<string> ReadInstruciton(string path)
+        {
+            return File.ReadAllLines(path).ToList();
+        }
+    }
+}
