2fd0cec434198bddc1729ec83923887d368266c5
BingFengHung <joe94008@gmail.com>
2022-07-11 08:59:24 +0800

    <feat>: 建立 ViewModelBase 類別

--- src/InstructionSearch/Frameworks/ViewModelBase.cs
+++ src/InstructionSearch/Frameworks/ViewModelBase.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
 
 namespace InstructionSearch
 {
-    class ViewModelBase
+    class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
