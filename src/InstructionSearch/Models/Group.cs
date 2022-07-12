311a06594b49990c2a5d1402bf7befc0cd5e67e8
BingFengHung <joe94008@gmail.com>
2022-07-12 16:07:48 +0800

    <refactor>: Clean Code

--- src/InstructionSearch/Models/Group.cs
+++ src/InstructionSearch/Models/Group.cs
@@ -1,7 +1,16 @@
 ﻿namespace InstructionSearch
 {
+    enum ItemType
+    {
+        Directory,
+        File
+    }
     class Group
     {
         public string Name { get; set; }
+
+        public ItemType Type { get; set; }
+
+        public string FullPath { get; set; }
     }
 }
