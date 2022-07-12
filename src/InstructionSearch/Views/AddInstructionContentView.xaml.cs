b5575db80ce8ee77daaee0ad840a872885fa5947
BingFengHung <joe94008@gmail.com>
2022-07-12 16:09:02 +0800

    <feat>: 加入新增指令集內容的類別

---
+++ src/InstructionSearch/Views/AddInstructionContentView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace InstructionSearch.Views
+{
+    /// <summary>
+    /// Interaction logic for AddInstructionContentView.xaml
+    /// </summary>
+    public partial class AddInstructionContentView : Window
+    {
+        public AddInstructionContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
